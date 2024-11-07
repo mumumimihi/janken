9fbe79d5722516f7d1cfc206bc4bd80094a70027
sawafujiouta <o.sawafuji.sys24@morijyobi.ac.jp>
2024-11-07 11:44:58 +0900

    c,dさんの作業終わりました

--- janken.py
+++ janken.py
@@ -1,1 +1,11 @@
 
+computer_hand = random.choice(hands)
+
+if user_hand == computer_hand:
+        print("引き分けです。")
+    elif win_rules[user_hand] == computer_hand:
+        print("あなたの勝ちです！")
+        user_wins += 1
+    else:
+        print("コンピュータの勝ちです。")
+        computer_wins += 1
